baad97f4ee0f053caf63e7ca86d46cce92b791f0
Roy Hyunjin Han <support@invisibleroads.com>
2011-10-06 00:26:10 -0400

    Added simple tool for transferring email between IMAP accounts

--- utilities/revive.py
+++ utilities/revive.py
@@ -1,16 +1,37 @@
 import os
 import sys
+import logging
+import datetime
 from argparse import ArgumentParser
+from calendar import timegm
 from ConfigParser import ConfigParser, NoSectionError
-from formencode import Invalid, Schema
-from formencode.validators import Int, String, StringBool
+from email.utils import mktime_tz, parseaddr, parsedate_tz
+from formencode import Invalid, Schema, validators
 from imapIO import IMAP4, IMAP4_SSL, IMAPError
+from sqlalchemy import Column, Date, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
 BASE_PATH = os.path.dirname(__file__)
 EXPAND_PATH = lambda x: os.path.join(BASE_PATH, x)
 SCRIPT_NAME = os.path.splitext(os.path.basename(__file__))[0]
 DESCRIPTION = 'Revive messages from source mailbox to target mailbox'
+RAW_FROM_LEN_MAX = 32
+RAW_DATE_LEN_MAX = 32
+
+
+Base = declarative_base()
+Session = sessionmaker()
+db = Session()
+
+
+class Message(Base):
+    __tablename__ = 'messages'
+    id = Column(Integer, primary_key=True)
+    raw_from = Column(String(RAW_FROM_LEN_MAX))
+    raw_date = Column(String(RAW_DATE_LEN_MAX))
+    date = Column(Date)
 
 
 class ApplicationError(Exception):
@@ -19,11 +40,11 @@ class ApplicationError(Exception):
 
 class IMAPForm(Schema):
 
-    host = String
-    port = Int
-    username = String
-    password = String
-    ssl = StringBool
+    host = validators.String
+    port = validators.Int
+    username = validators.String
+    password = validators.String
+    ssl = validators.StringBool
 
 
 def parse_args():
@@ -70,26 +91,96 @@ def connect(parameterByKey):
         parameterByKey['password'])
 
 
-args = parse_args()
-configParser = ConfigParser()
-try:
-    configParser.read(args.configurationPath)
-    configParser.read('.' + args.configurationPath)
-except Exception, error:
-    print 'Could not parse %s' % args.configurationPath
-    print error
-    sys.exit(1)
-try:
-    sourceParameterByKey = load_parameterByKey('source', configParser)
-    targetParameterByKey = load_parameterByKey('target', configParser)
-except ApplicationError, error:
-    print error
-    sys.exit(1)
-try:
-    sourceServer = connect(sourceParameterByKey)
-    targetServer = connect(targetParameterByKey)
-except IMAPError, error:
-    print error
-    sys.exit(1)
-# Load a message from the source mailbox
-# Revive message in target mailbox
+def parse_whenLocal(message):
+    timePack = parsedate_tz(message['date'])
+    if not timePack:
+        return
+    timeStamp = timegm(timePack) if timePack[-1] is None else mktime_tz(timePack)
+    return datetime.datetime.fromtimestamp(timeStamp)
+
+
+def has(server, message):
+    'Return True if the IMAP server has a copy of the message'
+    messageFrom = message['fromWhom']
+    messageDate = message['date']
+    whenLocal = parse_whenLocal(message)
+    # Without a date, I cannot easily test for duplicates
+    if not whenLocal:
+        return False
+    searchCriterion = 'FROM "%s" SENTON "%s"' % (parseaddr(messageFrom)[1], whenLocal.strftime('%d-%b-%Y'))
+    messageGenerator = server.walk(searchCriterion=searchCriterion)
+    for m in messageGenerator:
+        if m['date'] == messageDate:
+            return True
+    return False
+
+
+def has_record(message):
+    'Return True if we have already have a record of this message'
+    result = db.query(Message.id).filter(
+        (Message.raw_from==message['fromWhom'][:RAW_FROM_LEN_MAX]) &
+        (Message.raw_date==message['date'][:RAW_DATE_LEN_MAX])).first()
+    return True if result else False
+
+
+def record(message):
+    'Record the message in our database'
+    db.add(Message(
+        raw_from=message['fromWhom'][:RAW_FROM_LEN_MAX], 
+        raw_date=message['date'][:RAW_DATE_LEN_MAX],
+        date=parse_whenLocal(message).date()))
+    db.commit()
+
+
+
+if '__main__' == __name__:
+
+
+    args = parse_args()
+    configParser = ConfigParser()
+    try:
+        configParser.read(args.configurationPath)
+        configParser.read('.' + args.configurationPath)
+    except Exception, error:
+        print 'Could not parse %s' % args.configurationPath
+        print error
+        sys.exit(1)
+    try:
+        sourceParameterByKey = load_parameterByKey('source', configParser)
+        targetParameterByKey = load_parameterByKey('target', configParser)
+    except ApplicationError, error:
+        print error
+        sys.exit(1)
+
+
+    log = logging.getLogger('imapIO')
+    log.addHandler(logging.StreamHandler())
+    try:
+        sourceServer = connect(sourceParameterByKey)
+        targetServer = connect(targetParameterByKey)
+    except IMAPError, error:
+        print error
+        sys.exit(1)
+
+
+    engine = create_engine('sqlite:///%(username)s@%(host)s.db' % sourceParameterByKey)
+    db.configure(bind=engine)
+    Base.metadata.bind = engine
+    Base.metadata.create_all(engine)
+
+
+    # Get most recent
+    result = db.query(Message.date).order_by(Message.date.desc()).first()
+    searchCriterion = 'SINCE %s' % result[0].strftime('%d-%b-%Y') if result and args.incremental else ''
+    # Load a message from the source mailbox
+    for email in sourceServer.walk(searchCriterion=searchCriterion):
+        # If we already have a record of this email,
+        if has_record(email):
+            continue
+        # If the message does not exist in the target mailbox,
+        if not has(targetServer, email):
+            print email['subject'].encode('utf-8')
+            # Revive message in target mailbox
+            targetServer.revive(email.folder, email)
+            # Record email in our local database
+            record(email)
