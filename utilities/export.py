f1868e06bec565021cf34eea80a362eaace809fe
Roy Hyunjin Han <support@invisibleroads.com>
2011-10-20 12:19:07 -0400

    Whoops

--- utilities/export.py
+++ utilities/export.py
@@ -122,7 +122,7 @@ for result in messageQuery:
         print '[exists]'
     else:
         messageDate = message['date']
-        r, data = imapServer.append('inbox', '', mktime_tz(parsedate_tz(messageDate)) if messageDate else None, message)
+        r, data = imapServer.append('inbox', '', mktime_tz(parsedate_tz(messageDate)) if messageDate else None, message.as_string())
         if r != 'OK':
             raise Exception(data)
         print '[ok]'
